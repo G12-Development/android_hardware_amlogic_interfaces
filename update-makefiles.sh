ac978967dc0d75147faddcadcefd1a71ee0f0ab9
Aaron Kling <webgeek1234@gmail.com>
2021-07-07 20:44:13 -0500

    Restructure to allow interfaces in other vendor.amlogic namespaces
    Involved running an updated update-makefiles script

--- update-makefiles.sh
+++ update-makefiles.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-source system/tools/hidl/update-makefiles-helper.sh
+source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
 
 do_makefiles_update \
-  "vendor.amlogic.hardware:vendor/amlogic/common/interfaces" \
-  "android.hidl:system/libhidl/transport" \
-  "android.hardware:hardware/interfaces"
-  
-
-
+  "vendor.amlogic:hardware/amlogic/interfaces" \
+  "android.hardware:hardware/interfaces" \
+  "android.hidl:system/libhidl/transport"
